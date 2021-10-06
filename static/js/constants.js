d1fa768dec0b208073aa5b09cc87f4e2346e41a9
Aarbaz Alam <39804950+Aarbaz77@users.noreply.github.com>
2021-10-06 15:59:01 +0100

    Update constants.js

--- static/js/constants.js
+++ static/js/constants.js
@@ -1,5 +1,5 @@
 const action_name = "action_hello_world";
 // const rasa_server_url = "http://localhost:5055/webhook";
-const rasa_server_url = "http://34.125.94.107/core/webhooks/rest/webhook";
+const rasa_server_url = "http://172.31.15.62/core/webhooks/rest/webhook";
 // const rasa_server_url = "https://rasacouncildemo.herokuapp.com/webhooks/rest/webhook";
 const sender_id = uuidv4();
